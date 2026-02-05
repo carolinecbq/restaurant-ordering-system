dec6d3f26fe6a58e111ba78acada191105f39d95
Caroline <carolinecbq.12321@gmail.com>
2026-02-04 19:26:58 -0500

    Update Drink.h

--- Drink.h
+++ Drink.h
@@ -1,17 +1,3 @@
-/* Citation and Sources...
-Final Project Milestone 3:
-Module: Drink
-Filename: Drink.h
-Version 1.0
-Author: Caroline Chen, StNo: 112747241, Email: cchen247@myseneca.ca
-Revision History
------------------------------------------------------------
-Date      Reason
-
------------------------------------------------------------
-Citation to indicate that you have done all the work yourself
------------------------------------------------------------*/
-
 #ifndef DRINK_H
 #define DRINK_H
 #include <iostream>
