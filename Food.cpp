5b9fb838d027b8b871702c268d7812aec70c8af2
Caroline <carolinecbq.12321@gmail.com>
2026-02-04 19:27:07 -0500

    Update Food.cpp

--- Food.cpp
+++ Food.cpp
@@ -1,16 +1,3 @@
-/* Citation and Sources...
-Final Project Milestone 3:
-Module: Food
-Filename: Food.cpp
-Version 1.0
-Author: Caroline Chen, StNo: 112747241, Email: cchen247@myseneca.ca
-Revision History
------------------------------------------------------------
-Date      Reason
-
------------------------------------------------------------
-Citation to indicate that you have done all the work yourself
------------------------------------------------------------*/
 #include <iostream>
 #include <iomanip>
 #include <fstream>
