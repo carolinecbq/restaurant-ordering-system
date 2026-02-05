00f0aa9e575240f59882d3929a0f43fc3d2d6bf3
Caroline <carolinecbq.12321@gmail.com>
2026-02-04 19:27:55 -0500

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -1,30 +1,3 @@
-/***********************************************************************
-// OOP244 Project, Utils Module
-//
-// File	Utils.h
-// Version 0.1
-// started by Fardad 
-// Description
-// utility function to be used in the project
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-//
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
-/* Citation and Sources...
-Final Project Milestone 2:
-Module: Utils
-Filename: Utils.h
-Version 1.0
-Author: Caroline Chen, StNo: 112747241, Email: cchen247@myseneca.ca
-Revision History
------------------------------------------------------------
-Date      Reason
-2025/11/09 Added Milestone 2 functions
------------------------------------------------------------
-Citation to indicate that you have done all the work yourself
------------------------------------------------------------*/
 #ifndef SENECA_UTILS_H
 #define SENECA_UTILS_H
 namespace seneca {
